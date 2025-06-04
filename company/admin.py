2d291b9fed00a264d1a0b13abddb3d9cc1800ce7
Jude Woolls <jwoolls01@gmail.com>
2025-06-04 23:45:16 +0100

    register token model in admin.py

--- company/admin.py
+++ company/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Company, Venue, Coach, UserProfile
+from .models import Company, Venue, Coach, UserProfile, Token
 
 # Register your models here.
 admin.site.register(Company)
 admin.site.register(Venue)
 admin.site.register(UserProfile)
+admin.site.register(Token)
