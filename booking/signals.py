8178a2f5e97920ad458c3d0d9f94fbcf5b94befa
Jude Woolls <jwoolls01@gmail.com>
2025-06-05 18:00:26 +0100

    update the booking signal so that if a booking is deleted tokens are only returned for events in the future

--- booking/signals.py
+++ booking/signals.py
@@ -6,7 +6,17 @@ from company.models import Token  # Adjust path if needed
 @receiver(pre_delete, sender=Booking)
 def release_token_on_booking_delete(sender, instance, **kwargs):
     token = Token.objects.filter(booking=instance, used=True).first()
-    if token:
+    event = instance.event
+
+    if not token:
+        return
+
+    # add the event status update here
+
+    if event.status == 0:
         token.used = False
+        token.booking = None
+        token.save()
+    else:
         token.booking = None
         token.save()
